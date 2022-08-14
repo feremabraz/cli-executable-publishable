b235629acfc57f19de9a927bc18926475fdc284e
Fernando <fb@gamekaiju.monster>
2022-08-14 16:42:18 +0100

    CLI example.

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 #!/usr/bin/env node
+import fs from 'fs'
+import path from 'path'
+import url from 'url'
 
-console.log('It works.')
+let args = process.argv.slice(2)
+let argv = {}
+
+for (var i = 0; i < args.length; i++) {
+  var arg = args[i]
+  if (arg.indexOf('--') === 0) {
+    arg = arg.substring(2)
+    var index = arg.indexOf('=')
+    if (index > 0) {
+      argv[arg.substring(0, index)] = arg.substring(index + 1)
+    } else {
+      argv[arg] = true
+    }
+  }
+}
+
+if (argv.debug) {
+  console.log(argv)
+  console.log(args)
+}
+
+if (argv.help) {
+  console.log(
+    'Usage: cli-executable-publishable [--help] [--version] [--debug] [--hello <value>]',
+  )
+  process.exit(0)
+}
+
+if (argv.version) {
+  var filename = url.fileURLToPath(import.meta.url)
+  var packagePath = path.dirname(filename) + '/package.json'
+  var packageJSON = fs.readFileSync(packagePath)
+  var version = JSON.parse(packageJSON).version
+  console.log(version)
+  process.exit(0)
+}
+
+if (argv.hello) {
+  const value = args[args.findIndex((item) => item === '--hello') + 1]
+  console.log(`Hello ${value}`)
+  process.exit(0)
+}
+
+console.log('Unknown command.')
+process.exit(1)
